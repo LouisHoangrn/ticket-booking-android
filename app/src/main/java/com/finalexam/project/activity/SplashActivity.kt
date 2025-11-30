bae840b3c8893ce68a783ff59302c11d4a10fb0f
Hoang Long <louishoang205rn@gmail.com>
2025-11-30 22:14:05 +0700

    feat(project-setup): initialize splash screen and integrate Firebase
    - Added SplashActivity and splash_screen layout
    - Added gradient and background drawable resources
    - Updated AndroidManifest with new launcher activity
    - Added Firebase dependencies and configuration
    - Updated Gradle and settings files for Firebase setup
    - Added related string resources

--- app/src/main/java/com/finalexam/project/activity/SplashActivity.kt
+++ app/src/main/java/com/finalexam/project/activity/SplashActivity.kt
@@ -1,21 +1,23 @@
 package com.finalexam.project.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.finalexam.project.R
+import com.finalexam.project.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
