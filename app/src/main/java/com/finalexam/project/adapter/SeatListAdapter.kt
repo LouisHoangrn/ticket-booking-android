f97c434b9241a601c93b433a988d36afa4f0b024
Tran Van Luc <txcl3107@gmail.com>
2025-12-09 12:31:16 +0700

    merge dev into seat-list

---
+++ app/src/main/java/com/finalexam/project/adapter/SeatListAdapter.kt
@@ -0,0 +1,22 @@
+package com.finalexam.project.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SeatListAdapter():
+RecyclerView.Adapter<SeatListAdapter.Viewholder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SeatListAdapter.Viewholder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SeatListAdapter.Viewholder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
